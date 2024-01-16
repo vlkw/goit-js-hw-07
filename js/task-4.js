7d24bdd095efba2184b9b21c1185c92f56bfd90f
vlkw <138374305+vlkw@users.noreply.github.com>
2024-01-16 15:27:45 +0300

    1

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
-
+'use strict'
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+    if (emailValue === '' || passwordValue === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+    console.log(formData);
+    loginForm.reset();
+});
